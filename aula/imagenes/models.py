d9bf8a700481044ac4fbd8730a255f2031051d39
Daniel Castro <dancassig@gmail.com>
2016-04-26 16:56:43 -0500

    Removes artist

--- aula/imagenes/models.py
+++ aula/imagenes/models.py
@@ -9,7 +9,3 @@ class Image(models.Model):
     path = models.CharField(max_length=255)
     server = models.CharField(max_length=255)
 
-class Artist(models.Model):
-    name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=150)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
