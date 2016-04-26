ffad9ac61c68dbd12d29539be90f4179f826d2bb
Daniel Castro <dancassig@gmail.com>
2016-04-26 16:59:56 -0500

    Adds images and songs reference

--- aula/usuarios/models.py
+++ aula/usuarios/models.py
@@ -1,10 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from musica.models import Song
+from imagenes.models import Image
 
 
 # Create your models here.
-class User(models.Model):
-    Name = models.CharField(max_length=50)
+class Usersys(models.Model):
+    name = models.CharField(max_length=50)
     last_names = models.CharField(max_length=100)
     date_added = models.DateTimeField('Fecha de alta')
+    songs = models.ManyToManyField(Song, default=None)
+    images = models.ManyToManyField(Image, default=None)
+
+    def __str__(self):
+        return self.name
+
