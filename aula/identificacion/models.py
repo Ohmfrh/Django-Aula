2c17ac829c76c6235f0282e1669c15a535c90d0b
Daniel Castro <dancassig@gmail.com>
2016-04-26 16:58:14 -0500

    Adds user to id

--- aula/identificacion/models.py
+++ aula/identificacion/models.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from usuarios.models import Usersys
 
 
 # Create your models here.
 class Identify(models.Model):
     string = models.CharField(max_length=255)
+    usersys = models.ForeignKey(Usersys, on_delete=models.CASCADE, default=None)
 
 class Type():
     name = models.CharField(max_length=50)
-    identify = models.ForeignKey(Identify, on_delete=models.CASCADE)
+    #identify = models.ForeignKey(Identify, on_delete=models.CASCADE)
