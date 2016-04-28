c9a3770ecdd2b7ec52e6065b8eb7959e4bbf4a16
Daniel Castro <dancassig@gmail.com>
2016-04-27 21:15:06 -0500

    Adds crispy form to template

--- aula/usuarios/views.py
+++ aula/usuarios/views.py
@@ -1,17 +1,38 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Usersys
+
+from .forms import NameForm
+
 
 # Create your views here.
 def index(request):
     users = Usersys.objects.all()
-    context = {'users': users}
 
+    # print request
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+
+        # check whether it's valid:
+        if form.is_valid():
+            print request.POST
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/usuarios/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    context = {'users': users, 'form': form}
     return render(request, 'usuarios/index.html', context)
+
 
 def nuevo(request):
-    #print request
-    print "NUEVOOO"
-    users = Usersys.objects.all()
-    context = {'users': users}
-
+    print request
+    context = ""
     return render(request, 'usuarios/index.html', context)
+
