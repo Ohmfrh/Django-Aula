616b8d1913d147edfa3c20b5cb1a0f3c944594bb
Daniel Castro <dancassig@gmail.com>
2016-04-28 21:43:46 -0500

    Creates response to add new RFID with form

--- aula/identificacion/views.py
+++ aula/identificacion/views.py
@@ -1,9 +1,49 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from usuarios.models import Usersys
+
+from .models import Type, Identify
+from .forms import AddId
 
 
 # Create your views here.
 def index(request):
     users = Usersys.objects.all()
-    context = {'users': users}
+
+    form = AddId()
+
+    context = {'users': users, 'form': form}
+    return render(request, 'identificacion/index.html', context)
+
+
+def nuevo(request):
+    users = Usersys.objects.all()
+
+    # print request
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = AddId(request.POST)
+
+        # check whether it's valid:
+        if form.is_valid():
+            rfid = Type.objects.get(name='rfid')
+            user = Usersys.objects.get(pk=request.POST['Usuario'])
+            new = Identify(string=request.POST['String'], usersys=user, type=rfid)
+            new.save()
+
+            print rfid
+            print user
+
+
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/identificacion/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = AddId()
+
+    context = {'users': users, 'form': form}
     return render(request, 'identificacion/index.html', context)
