71ae802b7b970c67f1db5b372ed8121076d8b05e
Daniel Castro <dancassig@gmail.com>
2016-04-30 17:20:59 -0500

    Adds foreign key of Server to Image

--- aula/imagenes/models.py
+++ aula/imagenes/models.py
@@ -4,15 +4,6 @@ from django.db import models
 
 
 # Create your models here.
-class Image(models.Model):
-    name = models.CharField(max_length=255)
-    path = models.CharField(max_length=255)
-    server = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
 class Server(models.Model):
     address = models.CharField(max_length=100)
     user = models.CharField(max_length=50)
@@ -20,3 +11,12 @@ class Server(models.Model):
 
     def __str__(self):
         return self.address
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+    server = models.ForeignKey(Server, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
