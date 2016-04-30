362d3847090083a67e7c27cb56e2e64923f5bcef
Daniel Castro <dancassig@gmail.com>
2016-04-30 18:05:05 -0500

    Creates structure to return user image list

--- aula/imagenes/views.py
+++ aula/imagenes/views.py
@@ -1,6 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from usuarios.models import Usersys
+from imagenes.models import Image
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'imagenes/index.html')
+    users = Usersys.objects.all()
+    images = Image.objects.all()
+    context = {'users': users, 'images': images}
+    return render(request, 'imagenes/index.html', context)
+
+
+def usuario(request):
+    data = 'hola'
+    HttpResponse(data)
