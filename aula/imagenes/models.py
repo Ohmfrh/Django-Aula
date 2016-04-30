54beca8efa82462a69ed0e0da7a5506357ec3945
Daniel Castro <dancassig@gmail.com>
2016-04-30 17:13:11 -0500

    Creates new server model

--- aula/imagenes/models.py
+++ aula/imagenes/models.py
@@ -11,3 +11,12 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Server(models.Model):
+    address = models.CharField(max_length=100)
+    user = models.CharField(max_length=50)
+    password = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.address
