601bc5fc2cbf17343cc140d07318f72cd09c3f50
Daniel Castro <dancassig@gmail.com>
2016-04-30 15:24:18 -0500

    Adds urls to edit and add

--- aula/identificacion/urls.py
+++ aula/identificacion/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url('^nuevo/$', views.nuevo, name='nuevo'),
+    url('^editar/$', views.editar, name='editar'),
+    url('^identificar/$', views.identificar, name='identificar'),
 ]
