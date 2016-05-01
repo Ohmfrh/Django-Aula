4be7556440f5c977e3ae2d578ee8195c42b2afc2
Daniel Castro <dancassig@gmail.com>
2016-04-30 19:16:15 -0500

    Adds spacing

--- aula/imagenes/models.py
+++ aula/imagenes/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from usuarios.models import Usersys
 
+
 # Create your models here.
 class Server(models.Model):
     address = models.CharField(max_length=100)
