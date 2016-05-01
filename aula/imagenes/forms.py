4d23c131f4cc58d2a5b16922cc7153cffae8de84
Daniel Castro <dancassig@gmail.com>
2016-05-01 01:43:46 -0500

    Creates assign image form

--- aula/imagenes/forms.py
+++ aula/imagenes/forms.py
@@ -5,7 +5,7 @@ from crispy_forms.layout import Submit, Layout, Field
 from crispy_forms.bootstrap import (
     PrependedText, PrependedAppendedText, FormActions)
 
-from imagenes.models import Server
+from imagenes.models import Server, Image
 
 class AddImage(forms.Form):
     Name = forms.CharField(required=True, label='Nombre del archivo')
@@ -16,3 +16,16 @@ class AddImage(forms.Form):
     helper.form_method = 'POST'
     helper.form_action = '/imagenes/agregar/'
     helper.add_input(Submit('Agregar', 'Agregar', css_class='btn-primary'))
+
+
+class ImageForm(forms.Form):
+    Imagenes = forms.ModelMultipleChoiceField(queryset=Image.objects.all(), widget=forms.CheckboxSelectMultiple)
+
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.form_action = '/imagenes/editar/'
+    helper.add_input(Submit('Agregar', 'Agregar', css_class='btn-primary'))
+    helper.layout = Layout(
+        Field('Imagenes', css_class="checkbox-inline"),
+    )
+    helper.html5_required = False
