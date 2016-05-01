a40262e07b3f5aeb8bffcde2f35b83b03455e46d
Daniel Castro <dancassig@gmail.com>
2016-05-01 00:34:26 -0500

    Makes animations faster

--- aula/static/js/imagenes.js
+++ aula/static/js/imagenes.js
@@ -2,24 +2,33 @@
  * Created by daniel on 30/04/16.
  */
 function showImageList(){
-    $('.toggle-class').fadeOut('fast', function (){
-        $('#full-image-list').fadeIn('fast');
+    $('.toggle-class').fadeOut(250, function (){
+        console.log(this);
+        console.log('DONE');
+
     });
 
-
+    setTimeout(function () {
+        $('#full-image-list').fadeIn('fast');
+    },250);
 }
+
 function showUserImages(userId){
-    $('.toggle-class').fadeOut('fast', function() {
+    $('.toggle-class').fadeOut(250, function() {
         data = {name:'test'};
         $.get( "/imagenes/usuario/",data, function (data){
             console.log('Success');
             console.log(data);
         });
-        $('#user-image-list').fadeIn('fast');
     });
+
+    setTimeout(function () {
+        $('#user-image-list').fadeIn('fast');
+    },250);
+
     console.log('User: '+userId);
 }
 
 function addNewImage() {
-    $('#add-new-image').slideDown(500);
+    $('#add-new-image').slideDown(250);
 }
