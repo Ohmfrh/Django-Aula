4446ca066d8bfad8dd0ec372d74f98967428530c
Daniel Castro <dancassig@gmail.com>
2016-05-01 18:14:01 -0500

    Updates images assigned

--- aula/imagenes/views.py
+++ aula/imagenes/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -24,12 +26,27 @@ def usuario(request):
     if request.method == 'POST':
         assign_image_form = ImageForm(request.POST)
         if assign_image_form.is_valid():
+            userId = request.POST['UserOwner']
+            user = Usersys.objects.get(pk=userId)
+            userImages = UserImage.objects.all()
+            userImages = userImages.filter(user=userId)
+
+            for img in userImages:
+                img.delete()
+
+            list = request.POST.getlist('Imagenes')
+            for item in list:
+                img = Image.objects.get(pk=item)
+                usrimg = UserImage(user=user, image=img)
+                usrimg.save()
+
 
             return HttpResponseRedirect('/imagenes/')
 
     else:
-        assign_image_form = AddImage()
+        assign_image_form = ImageForm()
 
+    print "FOUR"
     context = {'users': users, 'images': images, 'add_image_form': add_image_form, 'assign_image_form': assign_image_form}
     return render(request, 'imagenes/index.html', context)
 
@@ -63,6 +80,16 @@ def agregar(request):
 
 
 def lista(request):
-    data = "From View"
+    list = []
+    userId = request.GET['userId']
+    images = Image.objects.all()
+    images = images.filter(users=userId)
 
-    HttpResponse(data)
+    for row in images:
+        list.append ({'imageId': row.pk})
+
+    # data = serializers.serialize("json", list)
+    data = json.dumps(list)
+
+    # data['user'] = Usersys.objects.get(pk=userId)
+    return HttpResponse(data, content_type='application/json')
