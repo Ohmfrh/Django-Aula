d8051ec3c465762ec5809df89c1fdab26e16e8eb
Daniel Castro <dancassig@gmail.com>
2016-05-02 01:19:35 -0500

    Sends server list as context

--- aula/multimedia/views.py
+++ aula/multimedia/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from .models import Server
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'multimedia/index.html')
+    servers = Server.objects.all()
+
+    context = {'servers': servers}
+    return render(request, 'multimedia/index.html', context)
