9ca7c0dac9427b829ad716ae3f26492e2332300d
Daniel Castro <dancassig@gmail.com>
2016-05-02 01:52:08 -0500

    Creates edit server function

--- aula/static/js/multimediaServer.js
+++ aula/static/js/multimediaServer.js
@@ -2,5 +2,9 @@
  * Created by daniel on 1/05/16.
  */
 function addServer() {
-    console.log('Add Server')
+    console.log('Add Server');
+}
+
+function editServer() {
+    console.log('Edit Server');
 }
