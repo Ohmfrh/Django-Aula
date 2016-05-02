014c9f4f964c0d795c118e8a18bf101317b522e5
Daniel Castro <dancassig@gmail.com>
2016-05-01 21:22:27 -0500

    Changes reference to musica

--- aula/static/js/canciones.js
+++ aula/static/js/canciones.js
@@ -17,13 +17,13 @@ function showUserSongs(userId){
     $('.toggle-class').fadeOut(250);
     setTimeout(function () {
         data = {userId: userId};
-        $.get( "/imagenes/lista/",data, function (data){
+        $.get( "/musica/lista/",data, function (data){
             $('input:checkbox').removeAttr('checked');
 
             if (data.length != 0) {
                 for (var i=0; i<data.length; i++) {
                     console.log(data[i]);
-                    $('#id_Imagenes_'+data[i]['imageId']).prop('checked', true);
+                    $('#id_Canciones_'+data[i]['songId']).prop('checked', true);
                 }
             }
 
