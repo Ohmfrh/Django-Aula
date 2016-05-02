dd0130746ed2cb9682381beda5ae1c2bb96302f6
Daniel Castro <dancassig@gmail.com>
2016-05-01 21:21:42 -0500

    Adds urls to all views

--- aula/musica/urls.py
+++ aula/musica/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^usuario/$', views.usuario, name='usuario'),
+    url(r'^agregar/$', views.agregar, name='agregar'),
+    url(r'^lista/$', views.lista, name='lista'),
 ]
