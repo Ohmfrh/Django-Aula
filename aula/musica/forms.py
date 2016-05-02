f805cf3875b261de2eb333fd0c2047b53708f429
Daniel Castro <dancassig@gmail.com>
2016-05-01 20:37:20 -0500

    Adds required forms

---
+++ aula/musica/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
+from crispy_forms.bootstrap import (
+    PrependedText, PrependedAppendedText, FormActions)
+
+from .models import Song
+from multimedia.models import Server
+
+class AddSong(forms.Form):
+    Name = forms.CharField(required=True, label='Nombre del archivo')
+    ServerList = forms.ModelChoiceField(required=True, queryset=Server.objects.all())
+    Path = forms.CharField(required=True, label='Path')
+
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.form_action = '/musica/agregar/'
+    helper.add_input(Submit('Agregar', 'Agregar', css_class='btn-primary'))
+
+
+class ImageForm(forms.Form):
+    Canciones = forms.ModelMultipleChoiceField(queryset=Song.objects.all(), widget=forms.CheckboxSelectMultiple, required=False)
+    UserOwner = forms.CharField(required=True, label='')
+
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.form_action = '/musica/usuario/'
+    helper.add_input(Submit('Agregar', 'Agregar', css_class='btn-primary'))
+    helper.layout = Layout(
+        Field('Canciones', css_class="checkbox-inline"),
+        Field('UserOwner', css_class="checkbox-inline", type="hidden"),
+    )
+    helper.html5_required = False
